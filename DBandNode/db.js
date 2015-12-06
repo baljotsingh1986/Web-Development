fced3a34cb88727525af975f3e4d35eae6941687
Baljot Singh <baljotsingh1986@yahoo.com>
2015-12-06 07:50:45 -0800

    Update db.js

--- DBandNode/db.js
+++ DBandNode/db.js
@@ -1,23 +1,35 @@
-// http://52.10.150.58:3000/get
+//Hosting: http://52.10.150.58:3000/get
+
+/*File Name: db.js
+*Author: Baljot Singh
+*Purpose: To get requests from client side to display, update, insert, delete data into
+*         server side databse and send the information to client side accordingly as html page/plain text  
+*/
+
 var mySql = require('mysql');
 var pool = mySql.createPool({
         host  : 'localhost',
-        user  : 'student',
-        password: 'default',
-        database: 'student'
+        user  : 'user',
+        password: 'password',
+        database: 'database'
     });
 
 var express = require('express');
 
 var app = express();
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.set('port', 3000);
 
+//Helper Function to select data from database and build a html table
 function getData(res, next, str){
     pool.query("SELECT id, name, reps, weight, date, lbs FROM workouts", function(err, rows, fields) {
         if(err) {
@@ -31,7 +43,7 @@ function getData(res, next, str){
             table += "<td>" + rows[i].name + "</td>\n";
             table += "<td>" + rows[i].reps + "</td>\n";
             table += "<td>" + rows[i].weight + "</td>\n";
-            table += "<td>" + rows[i].date.toDateString() + "</td>\n";
+            table += "<td>" + rows[i].date.toUTCString().slice(0, -13) + "</td>\n";
             table += "<td>" + rows[i].lbs + "</td>\n";
             table += '<td><input name="'+rows[i].id+'" type="button" value="Delete" class="btn btn-default" onClick="del(this.name)"><input name="'+rows[i].id+' "type="button" value="Update" class="btn btn-default" onClick="update(this.name)"></td>\n';
             table += "</tr>\n";
@@ -46,10 +58,6 @@ function getData(res, next, str){
 //to reset the table
 app.get('/reset-table',function(req,res,next){
     var context = {};
-    var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-    app.engine('handlebars', handlebars.engine);
-    app.set('view engine', 'handlebars');
-
     pool.query("DROP TABLE IF EXISTS workouts", function(err){
             var createString = "CREATE TABLE workouts("+
                     "id INT PRIMARY KEY AUTO_INCREMENT,"+
@@ -59,118 +67,68 @@ app.get('/reset-table',function(req,res,next){
                     "date DATE,"+
                     "lbs BOOLEAN)";
             pool.query(createString, function(err){
-            context.tablereset = "<p><b>Table Reset</b></p>";
-            res.render('home',context);
-            })
+                context.tablereset = "<p><b>Table Reset</b></p>";
+                res.render('home',context);
+            });
     });
 });
 
-
 //To insert data into table
 app.get('/insert',function(req,res,next){
-        var handlebars = require('express-handlebars').create({defaultLayout:'doc'});
-        app.engine('handlebars', handlebars.engine);
-        app.set('view engine', 'handlebars');
         pool.query("INSERT INTO workouts (name,reps,weight,date,lbs) VALUES (?,?,?,?,?)", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs],         function(err, result){
         if(err){
             next(err);
             return;
         }
-        getData(res, next, 'data');
-    });
-});
-
-//To change the row to form
-app.get('/update-row',function(req,res,next){
-        var handlebars = require('express-handlebars').create({defaultLayout:'doc'});
-        app.engine('handlebars', handlebars.engine);
-        app.set('view engine', 'handlebars');
-        var context = {};
-        pool.query("SELECT id, name, reps, weight, date, lbs FROM workouts", function(err, rows, fields){
-        if(err) {
-            next(err);
-            return;
+        if(result.insertId){
+            res.type('text/plain');
+            id = result.insertId.toString();
+            res.send(id);
         }
-        var id=req.query.id;
-        var table = '<table class="table table-striped" > <tr> <th>Name</th> <th>Reps</th> <th>Weight</th> <th>Date</th> <th>lbs or kg</th><th>Action</th> </tr>';
-        for (var i = 0; i < rows.length; i++){
-            if(id == rows[i].id){
-                var day =rows[i].date.getDate();
-                var year =rows[i].date.getFullYear();
-                var month = rows[i].date.getMonth() + 1;
-                var date = year+'-'+month+'-'+day;
-
-                table += '<tr id="'+rows[i].id+'">\n';
-                table += '<td><input name="update" type="text" value=' + rows[i].name + ' class="form-control"></td>\n';
-                table += '<td><input name="update" type="text" value=' + rows[i].reps + ' class="form-control"></td>\n';
-                table += '<td><input name="update" type="text" value=' + rows[i].weight + ' class="form-control"></td>\n';
-                table += '<td>YYYY-MM-DD<input name="update" type="text" value="' + date + '"  class="form-control"></td>\n';
-                table += '<td><input name="update" type="text" value=' + rows[i].lbs + ' class="form-control"></td>\n';
-                table += '<td><input name="'+rows[i].id+' "type="button" value="Save" class="btn btn-default" onClick="save(this.name)"></td>\n';
-                table += "</tr>\n";
-            } else {
-                table += '<tr id="'+rows[i].id+'">\n';
-                table += "<td>" + rows[i].name + "</td>\n";
-                table += "<td>" + rows[i].reps + "</td>\n";
-                table += "<td>" + rows[i].weight + "</td>\n";
-                table += "<td>" + rows[i].date.toDateString() + "</td>\n";
-                table += "<td>" + rows[i].lbs + "</td>\n";
-                table += '<td><input name="'+rows[i].id+'" type="button" value="Delete" class="btn btn-default" onClick="del(this.name)"><input name="'+rows[i].id+' "type="button" value="Update" class="btn btn-default" onClick="update(this.name)"></td>\n';
-                table += "</tr>\n";
-            }
-        }
-        table += "</table>\n";
-        context.result = table;
-        res.render('data',context);
-                return;
     });
 });
 
 //To delete row from table
 app.get('/delete',function(req,res,next){
-        var handlebars = require('express-handlebars').create({defaultLayout:'doc'});
-        app.engine('handlebars', handlebars.engine);
-        app.set('view engine', 'handlebars');
-
         pool.query("DELETE FROM workouts WHERE id=?", [req.query.id], function(err, result){
-        if(err){
-            next(err);
-            return;
-        }
-
-        getData(res, next, 'data');
+            if(err){
+                next(err);
+                return;
+            }
+            if(result){
+                res.type('text/plain');
+                res.send("Success");
+            }
     });
 });
 
 //To update the tables
 app.get('/update',function(req,res,next){
-    var handlebars = require('express-handlebars').create({defaultLayout:'doc'});
-    app.engine('handlebars', handlebars.engine);
-    app.set('view engine', 'handlebars');
-
     pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=?", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs, req.query.id], function(err, result){
         if(err){
             next(err);
             return;
         }
 
-        getData(res, next, 'data');
+        if(result){
+            res.type('text/plain');
+            res.send("Success");
+        }
     });
 });
 
 //To display table
 app.get('/get', function(req,res,next) {
-    var handlebarsGet = require('express-handlebars').create({defaultLayout:'main'});
-    app.engine('handlebars', handlebarsGet.engine);
-    app.set('view engine', 'handlebars');
     getData(res, next, 'home');
 });
 
+//Page not found error
 app.use(function(req,res){
     res.status(404);
     res.render('404');
 });
 
+//Error for execucting querries
 app.use(function(err, req, res, next){
     console.error(err.stack);
     res.type('plain/text');
@@ -178,7 +136,7 @@ app.use(function(err, req, res, next){
     res.render('500');
 });
 
+//Start app
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
-
